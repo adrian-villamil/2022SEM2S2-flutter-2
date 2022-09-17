bfcb1cfcea438128b0928c7bebaa098fc292a4fe
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-09-16 21:19:27 -0500

    Se agregó un slider a la segunda fila y se creó la variable estatura.

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -9,8 +9,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double peso = 0.0;
-  int edad = 0;
+  double peso = 50;
+  int edad = 10;
+  double estatura = 166;
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +75,28 @@ class _HomePageState extends State<HomePage> {
                   color: Color.fromARGB(255, 114, 112, 112),
                   borderRadius: BorderRadius.circular(20.0),
                 ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Estatura"),
+                      Text(
+                        '${estatura.toString()}cm',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Slider(
+                          value: estatura,
+                          min: 50,
+                          max: 250,
+                          divisions: 200,
+                          onChanged: (double value) {
+                            setState(() {
+                              estatura = value;
+                            });
+                          })
+                    ]),
                 //color: Color.fromARGB(255, 114, 112, 112),
               ),
             ),
@@ -186,8 +209,52 @@ class _HomePageState extends State<HomePage> {
           ),
           GestureDetector(
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: ((context) => DetailPage())));
+              double valorCalculo = peso / (estatura * estatura);
+              String estado = '';
+              Color colorEstado = Colors.black;
+
+              if (valorCalculo < 18.5) {
+                estado = 'Bajo peso';
+                colorEstado = Colors.orange;
+              }
+
+              if (valorCalculo >= 18.5 && valorCalculo <= 24.9) {
+                estado = 'Normal';
+                colorEstado = Colors.green;
+              }
+
+              if (valorCalculo >= 25 && valorCalculo <= 29.9) {
+                estado = 'Sobrepeso';
+                colorEstado = Color.fromARGB(255, 175, 112, 17);
+              }
+
+              if (valorCalculo >= 30 && valorCalculo <= 34.9) {
+                estado = 'Obesidad I';
+                colorEstado = Color.fromARGB(255, 214, 41, 41);
+              }
+
+              if (valorCalculo >= 35 && valorCalculo <= 39.9) {
+                estado = 'Obesidad II';
+                colorEstado = Color.fromARGB(255, 214, 41, 41);
+              }
+
+              if (valorCalculo >= 40 && valorCalculo <= 49.9) {
+                estado = 'Obesidad III';
+                colorEstado = Color.fromARGB(255, 141, 4, 4);
+              }
+
+              if (valorCalculo > 50) {
+                estado = 'Obesidad IV';
+                colorEstado = Color.fromARGB(255, 141, 4, 4);
+              }
+
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: ((context) => DetailPage(
+                          estado: estado,
+                          colorEstado: colorEstado,
+                          valor: valorCalculo))));
             },
             child: Container(
               height: 100,
